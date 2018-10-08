68ddc60e5deee0720e6c6ae091e1eb951ff0e0ba
cit 230 <flo15010@byui.edu>
2018-10-08 13:16:15 -0600

    messages implemented

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+
+  @ViewChild('subject') subject: ElementRef;
+  @ViewChild('msgText') msgText: ElementRef;
+
+  currentSender: string = 'Brandon';
+
+  @Output() addMessageEvent = new EventEmitter<Message>();
+
+  onSendMessage(){
+    const subjectValue = this.subject.nativeElement.value;
+    const msgTextValue = this.msgText.nativeElement.value;
+    const newMsg: Message = new Message(1, subjectValue, msgTextValue, this.currentSender);
+
+    this.addMessageEvent.emit(newMsg);
+
+    
+  }
+
+  onClear(){
+    this.subject.nativeElement.value = "";
+    this.msgText.nativeElement.value = "";
+  }
 
   constructor() { }
 
